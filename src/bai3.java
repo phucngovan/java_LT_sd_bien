a36f9285f4b0b03bcb0bda3cb3f77b3daa31533d
Win10 <phucdepzai0712@gmail.com>
2018-10-03 16:56:18 +0700

    sua loi

--- src/bai3.java
+++ src/bai3.java
@@ -6,8 +6,8 @@ public class bai3 {
         double pi = 3.14;
         Scanner sc = new Scanner(System.in);
         System.out.println("nhập vào đường kính hình tròn ");
-        d = sc.nextDouble();
-        s = pi * Math.pow(d, 2) / 4;
+        d = Double.parseDouble(sc.nextLine());
+        s = pi * d*d / 4;
         c = pi * d;
         System.out.println("chu vi hình tròn là " + c);
         System.out.println("diện tích hình tròn là " + s);
